77ea82d6cc8788be9d70fa30ded95570af5d894f
Hoang Le <artstyle0416@gmail.com>
2021-09-23 03:19:07 +0700

    init

---
+++ components/SearchBar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <>
+      <div className="styles.coin_search">
+        <input type="text" className="styles.coin_input" />
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
